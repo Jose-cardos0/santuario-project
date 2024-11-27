4ee09d17154f90fa2cc2aac37ca5468761556ae3
Jose Cardoso <josedeveloperJS@gmail.com>
2024-11-27 11:36:59 -0300

    correcoes finais

--- src/Pages/Adoracao.tsx
+++ src/Pages/Adoracao.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 
+import { AnimatedY } from "../tools/Animations/AnimatedY";
+
 interface propsSalmos {
   referencia: string;
   refrao: string;
@@ -29,6 +31,7 @@ interface propsAntifonas {
 
 interface propsResponse {
   data: string;
+  cor: string;
   liturgia: string;
   dia: string;
   oferendas: string;
@@ -59,58 +62,66 @@ export function Adoracao() {
   return (
     <section
       className="flex items-center justify-center
-     m-auto h-screen bg-cutom-gray-strong"
+     m-auto h-full bg-cutom-gray-strong mm:max-md:px-5"
     >
       {dataProps ? (
-        <div
-          className="font-roboto
-         max-w-3xl mt-28 bg-custom-gray p-8 rounded-lg shadow-lg 
+        <AnimatedY>
+          <div
+            className="font-roboto
+         max-w-3xl my-48 bg-custom-gray p-8 rounded-lg shadow-lg 
+          mm:max-md:my-32 mm:max-md:shadow-none
          "
-        >
-          <div>
-            <h1>Data: {dataProps?.data}</h1>
-            <p className="border-b-2 border-gray-300"></p>
-            <h1 className="my-4">
-              <strong className="text-2xl">Liturgia:</strong> <br />
-              <strong className="font-extralight">{dataProps?.liturgia}</strong>
-            </h1>
-            <div>
-              <p>{dataProps?.dia}</p>
-              <p>{dataProps?.oferendas}</p>
-              <p>{dataProps?.comunhao}</p>
+          >
+            <div className="">
+              <button className="flex items-center justify-center bg-green-500 rounded-lg px-2 font-bold text-white mb-2 shadow-md">
+                Cor litúrgica: {dataProps?.cor}
+              </button>
+              <h1>Data: {dataProps?.data}</h1>
+              <p className="border-b-2 border-gray-300"></p>
+              <h1 className="my-4">
+                <strong className="text-2xl">Liturgia:</strong> <br />
+                <strong className="font-extralight">
+                  {dataProps?.liturgia}
+                </strong>
+              </h1>
+              <div>
+                <p>{dataProps?.dia}</p>
+                <p>{dataProps?.oferendas}</p>
+                <p>{dataProps?.comunhao}</p>
+              </div>
+            </div>
+            <div className="mt-6">
+              <h1 className="mb-4">
+                <strong className="text-2xl">Salmo:</strong> <br />
+                <strong className="font-extralight">
+                  {dataProps?.salmo.referencia}
+                </strong>
+              </h1>
+              <div>
+                <p className="mb-4">
+                  <strong>Refrão:</strong> <br />
+                  {dataProps?.salmo.refrao}
+                </p>
+                <p>{dataProps?.salmo.texto}</p>
+              </div>
+            </div>
+            <div className="mt-6">
+              <h1 className="mb-4">
+                <strong className="text-2xl">Evangelho:</strong> <br />
+                <strong className="font-extralight">
+                  {dataProps?.evangelho.referencia}
+                </strong>
+              </h1>
+              <div>
+                <p className="mb-4">
+                  <strong>Título:</strong> <br />
+                  {dataProps?.evangelho.titulo}
+                </p>
+                <p>{dataProps?.evangelho.texto}</p>
+              </div>
             </div>
           </div>
-          <div className="mt-6">
-            <h1 className="mb-4">
-              <strong className="text-2xl">Salmo:</strong> <br />
-              <strong className="font-extralight">
-                {dataProps?.salmo.referencia}
-              </strong>
-            </h1>
-            <div>
-              <p className="mb-4">
-                <strong>Refrão:</strong> <br />
-                {dataProps?.salmo.refrao}
-              </p>
-              <p>{dataProps?.salmo.texto}</p>
-            </div>
-          </div>
-          <div className="mt-6">
-            <h1 className="mb-4">
-              <strong className="text-2xl">Evangelho:</strong> <br />
-              <strong className="font-extralight">
-                {dataProps?.evangelho.referencia}
-              </strong>
-            </h1>
-            <div>
-              <p className="mb-4">
-                <strong>Título:</strong> <br />
-                {dataProps?.evangelho.titulo}
-              </p>
-              <p>{dataProps?.evangelho.texto}</p>
-            </div>
-          </div>
-        </div>
+        </AnimatedY>
       ) : (
         <div>Loading...</div>
       )}
